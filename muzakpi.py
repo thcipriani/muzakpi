6eab73612485c87878d0de04c273ff43db556c42
Tyler Cipriani <tyler@tylercipriani.com>
2017-10-26 22:11:14 -0500

    Add play pause

--- muzakpi.py
+++ muzakpi.py
@@ -13,8 +13,24 @@ import dothat.touch as touch
 import requests
 
 
+JSON_PAYLOAD = {
+    'jsonrpc': '2.0',
+    'id': '1',
+}
+
+
+JSON_ENDPOINT = 'http://localhost:6680/mopidy/rpc'
+
+
 logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger()
+
+
+class YouTuber(object):
+    """
+    Flask app for adding tracks via youtube
+    """
+    pass
 
 
 class LcdScreen(object):
@@ -23,7 +39,12 @@ class LcdScreen(object):
     """
 
     def __init__(self):
-        pass
+        self.reset()
+
+    def reset(self):
+        lcd.clear()
+        bl.set_graph(0.0)
+        bl.off()
 
     def random_color(self, x):
         bl.sweep((x % 360) / 360.0)
@@ -48,6 +69,8 @@ class Control(object):
         Attempt to parse the json output of the modipy rpc track object.
         """
         result = track_info.get('result')
+        if result is None:
+            return None, None
         main_artist = result.get('artists', [{'name': 'Unknown Aritst'}])[0].get('name')
         title = result.get('name', 'Unknown Title')
         return main_artist, title
@@ -76,21 +99,39 @@ class Control(object):
         LOG.debug('TOUCHED LEFT!!!!!!!!!!!!!!!')
         previous_track()
 
+    @touch.on(touch.BUTTON)
+    def next_track(self):
+        LOG.debug('BUTTON TOUCHED!!!!!!!!!!!!!!!')
+        play_pause()
 
-JSON_PAYLOAD = {
-    'jsonrpc': '2.0',
-    'id': '1',
-}
-JSON_ENDPOINT = 'http://localhost:6680/mopidy/rpc'
+    @touch.on(touch.CANCEL)
+    def next_track(self):
+        LOG.debug('CANCEL TOUCHED!!!!!!!!!!!!!!!')
+
 
 def current_track():
     return _rpc_call()
 
+
 def next_track():
     return _rpc_call(method='core.playback.next')
 
+
 def previous_track():
     return _rpc_call(method='core.playback.previous')
+
+
+def play_pause():
+    if is_playing():
+        return _rpc_call(method='core.playback.pause')
+
+    return _rpc_call(method='core.playback.play')
+
+
+def is_playing():
+    state = _rpc_call(method='core.playback.get_state')
+    return state.get('result') == 'playing'
+
 
 def _rpc_call(method='core.playback.get_current_track'):
     payload = copy.deepcopy(JSON_PAYLOAD)
@@ -105,13 +146,19 @@ def main():
     controls = Control()
     x = 0
     while True:
-        artist, title = controls.current_track
-        LOG.debug('Aritst: {}\nTitle: {}\nX: {}'.format(artist, title, x))
+        if not is_playing():
+            lcd_screen.reset()
 
-        lcd_screen.random_color(x)
-        lcd_screen.show_track(artist, title)
+        else:
+            artist, title = controls.current_track
+            LOG.debug('Aritst: {}\nTitle: {}\nX: {}'.format(artist, title, x))
 
-        x += 10
+            lcd_screen.random_color(x)
+            lcd_screen.show_track(artist, title)
+
+            x += 10
+            if x >= 360:
+                x = 0
         time.sleep(2)
 
 if __name__ == '__main__':
